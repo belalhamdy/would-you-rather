775c42a6e230c044ec1a5e61c557b9abdb67257e
Belal Hamdy <belalhezzat@gmail.com>
2020-04-10 04:28:24 +0200

    Finishing reducers, actions and middlewares

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -10,15 +10,5 @@ class Nav extends Component{
         )
     }
 }
-function mapStateToProps ({authedUser, users, tweets}, { id }) {
-    /*const tweet = tweets[id];
-    const parentTweet = tweet ? tweets[tweet.replyingTo] : null;
 
-    return {
-        authedUser,
-        tweet: tweet
-            ? formatTweet(tweet, users[tweet.author], authedUser, parentTweet)
-            : null
-    }*/
-}
-export default connect(mapStateToProps)(Nav);
+export default Nav;
